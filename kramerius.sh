70df0974963cd8e0cb238ab3a176773e7e007bd1
stlouky <j.stloukal@gmail.com>
2017-01-01 22:05:38 +0100

    doplnění komentářů

--- kramerius.sh
+++ kramerius.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-#vstupy
+#první stránka dokumentu
 read -p "Vlož adresu první stránky a stiskni [ENTER]: " FIRST_URL
+#poslední stránka dokumentu
 read -p "Vlož poslední stránku a stiskni [ENTER]: " SECOND_URL
+#název vytvořeného dokumentu
 read -p "Vlož jméno dokumentua a stiskni [ENTER]: " NAME_TITLE
 PREFIX=2
 
 URL="${FIRST_URL%=*}"+"="
 
-#	subpage			#
+#podstránka		#
 START_SUBPAGE="${FIRST_URL##*=}"
 END_SUBPAGE="${SECOND_URL##*=}"
 SUBPAGE=$START_SUBPAGE
 
-#	page			#
+#první a poslední stránka#
 FIRST_PAGE="${FIRST_URL##*_}"
 FIRST_PAGE="${FIRST_PAGE%.*}"
 LAST_PAGE="${SECOND_URL##*_}"
 LAST_PAGE="${LAST_PAGE%.*}"
 
-#	core url		#
+#core url		#
 CORE_URL="${FIRST_URL%_*}"
 
 echo "Začínám stahovat soubory..."
 
-#stahován dokumentů
+#stahován dokumentu od prvního do posledního
 for((i = $FIRST_PAGE ; i <= $LAST_PAGE; i++));do
+	#jméno stahované stránky
 	OUT_NAME=$i".djvu"
+	#sestaveni adresy stránky a stažení do aktualního adresáře
 	wget ""$CORE_URL""_"$i".djvu?id="$START_SUBPAGE" -O "$OUT_NAME"
 	((START_SUBPAGE = START_SUBPAGE + $PREFIX))
 done
@@ -35,7 +39,9 @@ echo "Soubory byly staženy vytvářim dokument..."
 
 shopt -s extglob
 
+#jméno vytvářeného dokumentu
 OUTFILE=$NAME_TITLE.djvu
+#maska nazvu jednotlivých stránek
 DEFMASK="${OUT_NAME:0:4}*.djvu"
 
 if [-n "$1" ]; then
@@ -43,11 +49,11 @@ if [-n "$1" ]; then
 else
    MASK=$DEFMASK
 fi
-
+#vytvoření dokumentu
 djvm -c $OUTFILE $MASK
 
 echo "Dokument vytvořen, mažu soubory..."
-
+#smazaní nepotřebných stránek
 rm -r ${OUT_NAME:0:4}*.djvu
 
 echo "hotovo!!!"
