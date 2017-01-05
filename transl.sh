7f2586b22b0d4877a18f9f4e1ac12968c54a14a2
stlouky <j.stloukal@gmail.com>
2017-01-05 22:35:11 +0100

    Oprava: překlad mezi tagy <i> </i>

--- transl.sh
+++ transl.sh
@@ -4,18 +4,21 @@
 ## a připojení k internetu
 
 # Inicializace proměnných
-NAME_FILE_IN=""					#upravovany soubor s titulky
-NAME_FILE_OUT=""				#upravený soubor
-TARGET_LANG="cs"				#cílový jazyk
-SOURCE_LANG="en"				#zdrojový jazyk
+NAME_FILE_IN=""					        #upravovany soubor s titulky
+NAME_FILE_OUT=""				        #upravený soubor
+TARGET_LANG="cs"				        #cílový jazyk
+SOURCE_LANG="en"				        #zdrojový jazyk
 
 echo -n "Zadej jméno souboru: "			#prompt
-read NAME_FILE_IN				#přečte nazev souboru
-NAME_FILE_OUT=$TARGET_LANG"_"$NAME_FILE_IN	#vytvoři jmeno vystunpniho souboru
-touch $NAME_FILE_OUT				#vytvoří výstupní soubor
+read NAME_FILE_IN				        #přečte nazev souboru
 
-# Funkce vytvoří pole [array]
-# z vloženého textového souboru
+#vytvoři jmeno vystunpniho souboru
+NAME_FILE_OUT=$TARGET_LANG"_"$NAME_FILE_IN	
+touch $NAME_FILE_OUT				    #vytvoří výstupní soubor
+
+
+# Funkce vytvoří pole [array] z vloženého
+# textového souboru
 getArray(){
 	array=()
 	while IFS= read -r line
@@ -24,22 +27,34 @@ getArray(){
 	done < "$1"
 }
 
-getArray $NAME_FILE_IN				#zavolá funkci pro vytvoření pole
+#zavolá funkci pro vytvoření pole
+getArray $NAME_FILE_IN				
 echo "Začínám překládat...."
 
+# přeloží všechny řádky souboru
 for e in "${array[@]}"
 do
+   # přeloží a zapíše do soubouru text mezi tagy [<i>  </i>]
+   if [[ ${e:0:1} =~ "<" ]]; then
+        wor_d=${e:3}
+        wor_d=${wor_d::-5}
+        echo "<i> "$(translate-shell -brief "$wor_d" $SOURCE_LANG:$TARGET_LANG)" </i>" >> $NAME_FILE_OUT
+    fi
+   #echo $e
 	#podmínky
 	re='[a-z A-Z]'				
 	ra="'"
 	ri="-"
-	ro='"'
+	ro='"'	
        	# Vyhodnotí záznam pokud splňuje podmínky přeloží ho
 	if [[ ${e:0:1} =~ $re || ${e:0:1} =~ $ra || ${e:0:1} =~ $ri || ${e:0:1} =~ $ro ]]; then
 		echo $(translate-shell -brief "$e" $SOURCE_LANG:$TARGET_LANG) >> $NAME_FILE_OUT
 	else
-		# při nesplnění podmínky vloži nepřeložený text
-		echo $e >> $NAME_FILE_OUT
+		# při nesplnění podmínky zapíše do souboru nepřeložený text
+		# pokud řádka začína tagem <i> nezapíše nic
+		if [[ ${e:0:1} != "<" ]]; then
+            echo $e >> $NAME_FILE_OUT
+        fi
 	fi
 done
 
