50bbb6d63a6488404e49f0d0eb2273c6b64696af
stlouky <j.stloukal@gmail.com>
2017-01-07 01:45:33 +0100

    Oprava: překlad neuplných tagů <i...

--- transl.sh
+++ transl.sh
@@ -4,21 +4,26 @@
 ## a připojení k internetu
 
 # Inicializace proměnných
-NAME_FILE_IN=""					        #upravovany soubor s titulky
-NAME_FILE_OUT=""				        #upravený soubor
-TARGET_LANG="cs"				        #cílový jazyk
-SOURCE_LANG="en"				        #zdrojový jazyk
+NAME_FILE_IN=""                         #upravovany soubor
+NAME_FILE_OUT=""                        #upravený soubor
+TARGET_LANG="cs"                        #cílový jazyk
+SOURCE_LANG="en"                        #zdrojový jazyk
 
-echo -n "Zadej jméno souboru: "			#prompt
-read NAME_FILE_IN				        #přečte nazev souboru
+# patern tagy
+FI_TG="<i>" LA_TG="</i>" MEZ=" "
+# patern znaky [first char]
+re='[a-z A-Z]' ra="'" ri="-" ro='"'
 
-#vytvoři jmeno vystunpniho souboru
-NAME_FILE_OUT=$TARGET_LANG"_"$NAME_FILE_IN	
-touch $NAME_FILE_OUT				    #vytvoří výstupní soubor
+echo -n "Zadej jméno souboru: "         #prompt
+read NAME_FILE_IN                       #přečte nazev souboru
+
+# vytvoři jmeno vystunpniho souboru
+NAME_FILE_OUT=$TARGET_LANG"_"$NAME_FILE_IN
+#vytvoří výstupní soubor
+touch $NAME_FILE_OUT                    
 
 
-# Funkce vytvoří pole [array] z vloženého
-# textového souboru
+# Funkce vytvoří pole [array] z vloženého textového souboru
 getArray(){
 	array=()
 	while IFS= read -r line
@@ -27,35 +32,43 @@ getArray(){
 	done < "$1"
 }
 
-#zavolá funkci pro vytvoření pole
-getArray $NAME_FILE_IN				
+# zavolá funkci pro vytvoření pole
+getArray $NAME_FILE_IN
+
 echo "Začínám překládat...."
 
-# přeloží všechny řádky souboru
 for e in "${array[@]}"
 do
-   # přeloží a zapíše do soubouru text mezi tagy [<i>  </i>]
-   if [[ ${e:0:1} =~ "<" ]]; then
-        wor_d=${e:3}
-        wor_d=${wor_d::-5}
-        echo "<i> "$(translate-shell -brief "$wor_d" $SOURCE_LANG:$TARGET_LANG)" </i>" >> $NAME_FILE_OUT
-    fi
-   #echo $e
-	#podmínky
-	re='[a-z A-Z]'				
-	ra="'"
-	ri="-"
-	ro='"'	
-       	# Vyhodnotí záznam pokud splňuje podmínky přeloží ho
-	if [[ ${e:0:1} =~ $re || ${e:0:1} =~ $ra || ${e:0:1} =~ $ri || ${e:0:1} =~ $ro ]]; then
-		echo $(translate-shell -brief "$e" $SOURCE_LANG:$TARGET_LANG) >> $NAME_FILE_OUT
-	else
-		# při nesplnění podmínky zapíše do souboru nepřeložený text
-		# pokud řádka začína tagem <i> nezapíše nic
-		if [[ ${e:0:1} != "<" ]]; then
+    echo $e
+    # smaž vše od [>]
+    FIRST_TAG=${e:0:3}    
+    # smaž vše až do [<]
+    LAST_TAG=${e:(-5)}
+    
+    # prvni char
+    FIRST_CHAR=${e:0:1}
+        
+    # testuj na tag [<i>] [</i>]
+    if [[ $FIRST_TAG =~ $FI_TG || $LAST_TAG =~ $LA_TG ]]; then
+        # smaž tagy [nejdou přeložit]          
+        e="${e/$FI_TG/$MEZ}" 
+        e="${e/$LA_TG/$MEZ}"
+        
+        echo "<i>"$(translate-shell -brief "$e" $SOURCE_LANG:$TARGET_LANG)"</i>" >> $NAME_FILE_OUT
+    else    
+        # test na písmeno a interpunkci? prvního znaku [a-z A-Z] [' " - . ,]
+        if [[ $FIRST_CHAR =~ $re ||
+              $FIRST_CHAR =~ $ra ||
+              $FIRST_CHAR =~ $ri ||
+              $FIRST_CHAR =~ $ro ]]; then
+              
+            echo $(translate-shell -brief "$e" $SOURCE_LANG:$TARGET_LANG) >> $NAME_FILE_OUT 
+        else
+            # při nesplnění podmínky zapíše do souboru nepřeložený text
             echo $e >> $NAME_FILE_OUT
-        fi
-	fi
+        fi        
+    fi    
 done
 
-echo "Hotovo!!! "
+echo "Hotovo!!!"
+
