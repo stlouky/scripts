c576bf41346598271a11f30cd14bf70a1b7c74a2
stlouky <j.stloukal@gmail.com>
2017-01-08 23:32:55 +0100

    Oparava: výpočet procent

--- transl.sh
+++ transl.sh
@@ -12,7 +12,7 @@ SOURCE_LANG="en"                        #zdrojový jazyk
 # patern tagy
 FI_TG="<i>" LA_TG="</i>" MEZ=" "
 # patern znaky [first char]
-re='[a-z A-Z]' ra="'" ri="-" ro='"'
+re='[a-z A-Z]' ra="'" ri="-" ro='"' ry="."
 
 echo -n "Zadej jméno souboru: "         #prompt
 read NAME_FILE_IN                       #přečte nazev souboru
@@ -36,10 +36,14 @@ getArray(){
 getArray $NAME_FILE_IN
 
 echo "Začínám překládat...."
+COUNT=1
+COUNT_MAX=0 
+
+# spočítá prvky pole
+for i in "${array[@]}";do ((COUNT_MAX++)); done
 
 for e in "${array[@]}"
 do
-    echo $e
     # smaž vše od [>]
     FIRST_TAG=${e:0:3}    
     # smaž vše až do [<]
@@ -47,7 +51,9 @@ do
     
     # prvni char
     FIRST_CHAR=${e:0:1}
-        
+    
+    proc=$((COUNT*100/COUNT_MAX))
+    ((COUNT++))
     # testuj na tag [<i>] [</i>]
     if [[ $FIRST_TAG =~ $FI_TG || $LAST_TAG =~ $LA_TG ]]; then
         # smaž tagy [nejdou přeložit]          
@@ -55,19 +61,22 @@ do
         e="${e/$LA_TG/$MEZ}"
         
         echo "<i>"$(translate-shell -brief "$e" $SOURCE_LANG:$TARGET_LANG)"</i>" >> $NAME_FILE_OUT
-    else    
+        echo "[ $proc% ] "$e
+    else              
         # test na písmeno a interpunkci? prvního znaku [a-z A-Z] [' " - . ,]
         if [[ $FIRST_CHAR =~ $re ||
               $FIRST_CHAR =~ $ra ||
               $FIRST_CHAR =~ $ri ||
-              $FIRST_CHAR =~ $ro ]]; then
+              $FIRST_CHAR =~ $ro ||
+              $FIRST_CHAR =~ $ry ]]; then
               
             echo $(translate-shell -brief "$e" $SOURCE_LANG:$TARGET_LANG) >> $NAME_FILE_OUT 
+            echo "[ $proc% ] "$e
         else
             # při nesplnění podmínky zapíše do souboru nepřeložený text
             echo $e >> $NAME_FILE_OUT
         fi        
-    fi    
+    fi      
 done
 
 echo "Hotovo!!!"
