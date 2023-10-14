638e1bb51131e5f6b39fc1670506849f5ee04e7c
stlouky <j.stloukal@gmail.com>
2023-10-14 19:10:33 +0200

    Update kramerius.sh

--- kramerius.sh
+++ kramerius.sh
@@ -1,60 +1,62 @@
 #!/bin/bash
 
-#první stránka dokumentu
-read -p "Vlož adresu první stránky a stiskni [ENTER]: " FIRST_URL
-#poslední stránka dokumentu
-read -p "Vlož poslední stránku a stiskni [ENTER]: " SECOND_URL
-#název vytvořeného dokumentu
-read -p "Vlož jméno dokumentua a stiskni [ENTER]: " NAME_TITLE
+# Skript pro stahování DJVU stránek a vytvoření DJVU dokumentu
+
+# Zadejte adresu první stránky DJVU dokumentu
+read -p "Vložte adresu první stránky a stiskněte [ENTER]: " FIRST_URL
+
+# Zadejte adresu poslední stránky DJVU dokumentu
+read -p "Vložte adresu poslední stránky a stiskněte [ENTER]: " SECOND_URL
+
+# Zadejte jméno vytvořeného dokumentu
+read -p "Vložte jméno dokumentu a stiskněte [ENTER]: " NAME_TITLE
+
+# Příprava proměnných
 PREFIX=2
-
-URL="${FIRST_URL%=*}"+"="
-
-#podstránka		#
+URL="${FIRST_URL%=*}"
 START_SUBPAGE="${FIRST_URL##*=}"
 END_SUBPAGE="${SECOND_URL##*=}"
 SUBPAGE=$START_SUBPAGE
-
-#první a poslední stránka#
 FIRST_PAGE="${FIRST_URL##*_}"
 FIRST_PAGE="${FIRST_PAGE%.*}"
 LAST_PAGE="${SECOND_URL##*_}"
 LAST_PAGE="${LAST_PAGE%.*}"
-
-#core url		#
 CORE_URL="${FIRST_URL%_*}"
+
+# Funkce pro stahování jedné stránky
+function download_page() {
+    page_number=$1
+    OUT_NAME="${page_number}.djvu"
+    wget "${CORE_URL}_${page_number}.djvu?id=${START_SUBPAGE}" -O "${OUT_NAME}"
+    ((START_SUBPAGE = START_SUBPAGE + PREFIX))
+}
 
 echo "Začínám stahovat soubory..."
 
-#stahován dokumentu od prvního do posledního
-for((i = $FIRST_PAGE ; i <= $LAST_PAGE; i++));do
-	#jméno stahované stránky
-	OUT_NAME=$i".djvu"
-	#sestaveni adresy stránky a stažení do aktualního adresáře
-	wget ""$CORE_URL""_"$i".djvu?id="$START_SUBPAGE" -O "$OUT_NAME"
-	((START_SUBPAGE = START_SUBPAGE + $PREFIX))
+# Stahování dokumentu od prvního do posledního
+for ((i = FIRST_PAGE; i <= LAST_PAGE; i++)); do
+    download_page "$i"
 done
 
-echo "Soubory byly staženy vytvářim dokument..."
+echo "Soubory byly staženy, vytvářím dokument..."
 
+# Příprava pro vytvoření DJVU dokumentu
 shopt -s extglob
+OUTFILE="${NAME_TITLE}.djvu"
+DEFMASK="*.djvu"
 
-#jméno vytvářeného dokumentu
-OUTFILE=$NAME_TITLE.djvu
-#maska nazvu jednotlivých stránek
-DEFMASK="${OUT_NAME:0:4}*.djvu"
-
-if [-n "$1" ]; then
-   MASK=$1
+if [ -n "$1" ]; then
+    MASK="$1"
 else
-   MASK=$DEFMASK
+    MASK="$DEFMASK"
 fi
-#vytvoření dokumentu
-djvm -c $OUTFILE $MASK
 
-echo "Dokument vytvořen, mažu soubory..."
-#smazaní nepotřebných stránek
-rm -r ${OUT_NAME:0:4}*.djvu
+# Vytvoření DJVU dokumentu
+djvm -c "${OUTFILE}" $MASK
 
-echo "hotovo!!!"
+echo "Dokument vytvořen, mazu soubory..."
 
+# Smazání nepotřebných stránek
+rm -r *.djvu
+
+echo "Hotovo!!!"
