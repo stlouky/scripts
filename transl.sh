d5af2040ecffe814f31be68c343035a42a0aee16
stlouky <j.stloukal@gmail.com>
2025-01-06 18:45:04 +0000

    Update transl.sh

--- transl.sh
+++ transl.sh
@@ -1,91 +1,48 @@
 #!/bin/bash
 
-## vyžaduje nainstalovaný [translator-shell]
-## a připojení k internetu
+# Skript pro překlad titulků v souboru .srt z cizího jazyka do češtiny
 
-# Inicializace proměnných
-NAME_FILE_IN=""                         #upravovany soubor
-NAME_FILE_OUT=""                        #upravený soubor
-TARGET_LANG="cs"                        #cílový jazyk
-SOURCE_LANG="en"                        #zdrojový jazyk
-TRANS_ENGINE="bing" 			#google, bing,...
+# Zkontrolujte, zda je nainstalován nástroj trans
+if ! command -v trans &> /dev/null
+then
+    echo "Nástroj 'trans' není nainstalován. Instalujte jej před spuštěním tohoto skriptu."
+    echo "Postup instalace nástroje 'trans':"
+    echo "1. Ujistěte se, že máte nainstalovaný 'curl' a 'git'."
+    echo "   Můžete je nainstalovat pomocí příkazu: sudo apt-get install curl git"
+    echo "2. Stáhněte a nainstalujte 'translate-shell' pomocí těchto příkazů:"
+    echo "   git clone https://github.com/soimort/translate-shell.git"
+    echo "   cd translate-shell"
+    echo "   make"
+    echo "   sudo make install"
+    echo "3. Ověřte instalaci pomocí příkazu: trans -V"
+    exit 1
+fi
 
-# patern tagy
-FI_TG="<i>" LA_TG="</i>" MEZ=" "
-# patern znaky [first char]
-re='[a-z A-Z]' ra="'" ri="-" ro='"' ry="." nu='^[0-9]+$'
+# Zadejte název vstupního souboru .srt
+read -p "Zadejte cestu k souboru s titulky (.srt): " INPUT_FILE
 
-echo -n "Zadej jméno souboru: "         #prompt
-read NAME_FILE_IN                       #přečte nazev souboru
+# Zadejte název výstupního souboru .srt
+read -p "Zadejte cestu k uloženému přeloženému souboru (.srt): " OUTPUT_FILE
 
-# vytvoři jmeno vystunpniho souboru
-NAME_FILE_OUT=$TARGET_LANG"_"$NAME_FILE_IN
-#vytvoří výstupní soubor
-touch $NAME_FILE_OUT                    
+# Zkontrolujte, zda vstupní soubor existuje
+if [ ! -f "$INPUT_FILE" ]; then
+    echo "Zadaný soubor neexistuje."
+    exit 1
+fi
 
+# Překlad titulků
+echo "Začínám překládat titulky..."
 
-# Funkce vytvoří pole [array] z vloženého textového souboru
-getArray(){
-	array=()
-	while IFS= read -r line
-	do
-		array+=("$line")
-	done < "$1"
-}
-
-# zavolá funkci pro vytvoření pole
-getArray $NAME_FILE_IN
-
-echo "Začínám překládat...."
-COUNT=1
-COUNT_MAX=0 
-
-# spočítá prvky pole
-for i in "${array[@]}";do ((COUNT_MAX++)); done
-
-for e in "${array[@]}"
+while IFS= read -r line
 do
-    #pokud e obsahuje \r smaž
-    if [[ $e =~ $'\r' ]]; then
-	e=${e:0:-1}
+    # Pokud je řádek prázdný nebo obsahuje pouze čísla (časová razítka), nepřekládejte
+    if [[ -z "$line" || $line =~ ^[0-9]+$ || $line =~ --> ]]; then
+        echo "$line" >> "$OUTPUT_FILE"
+    else
+        # Přeložte řádek do češtiny
+        translated_line=$(echo "$line" | trans -b :cs)
+        echo "$translated_line" >> "$OUTPUT_FILE"
     fi
-    #echo "[1]"$e
-    # smaž vše od [>]
-    FIRST_TAG=${e:0:3}    
-    # smaž vše až do [<]
-    LAST_TAG=${e:(-5)}
-    
-    # prvni char
-    FIRST_CHAR=${e:0:1}
-    #echo "[2]"$e
-    proc=$((COUNT*100/COUNT_MAX))
-    ((COUNT++))
-    # testuj na tag [<i>] [</i>]
-    if [[ $FIRST_TAG =~ $FI_TG || $LAST_TAG =~ $LA_TG ]]; then
-        # smaž tagy [nejdou přeložit]          
-        e="${e/$FI_TG/$MEZ}" 
-        e="${e/$LA_TG/$MEZ}"
-        #echo "[3]"$e
-        echo "<i>"$(trans -brief -e $TRANS_ENGINE "$e" $SOURCE_LANG:$TARGET_LANG)"</i>" >> $NAME_FILE_OUT
-        echo "[ $proc% ] "$e
-    else              
-        # test na písmeno a interpunkci? prvního znaku [a-z A-Z] [' " - . ,]
-        if [[ $FIRST_CHAR =~ $re ||
-              $FIRST_CHAR =~ $ra ||
-              $FIRST_CHAR =~ $ri ||
-              $FIRST_CHAR =~ $ro ||
-              $FIRST_CHAR =~ $ry &&
-	      $FIRST_CHAR =~ !$nu ]]; then
-            #echo "[4]"$e  
-            echo $(trans -brief -e $TRANS_ENGINE "$e" $SOURCE_LANG:$TARGET_LANG) >> $NAME_FILE_OUT 
-            echo "[ $proc% ] "$e
-        else
-            # při nesplnění podmínky zapíše do souboru nepřeložený text
-	    #echo "[5]"$e
-	    echo $e >> $NAME_FILE_OUT
-        fi        
-    fi      
-done
+done < "$INPUT_FILE"
 
-echo "Hotovo!!!"
-
+echo "Překlad dokončen. Přeložené titulky byly uloženy do souboru: $OUTPUT_FILE"
